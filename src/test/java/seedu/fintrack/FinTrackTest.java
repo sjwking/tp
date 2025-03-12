0885ae8cc747a367b54229261a8dd66ec007aa0a
Akaash Dhayalan <116476469+akaash02@users.noreply.github.com>
2025-03-12 14:41:55 +0800

    Revert "Set up test files."

--- src/test/java/seedu/fintrack/FinTrackTest.java
+++ src/test/java/seedu/fintrack/FinTrackTest.java
@@ -19,49 +19,37 @@ public class FinTrackTest {
     @Test
     public void testHandleExpense() {
         FinTrack.handleExpense();
-        assertEquals("Adding expense...\n",
-                outContent.toString(),
-                "handleExpense() should print 'Adding expense...'");
+        assertEquals("Adding expense...\n", outContent.toString(), "handleExpense() should print 'Adding expense...'");
     }
 
     @Test
     public void testCheckHistory() {
         FinTrack.checkHistory();
-        assertEquals("Checking history...\n",
-                outContent.toString(),
-                "checkHistory() should print 'Checking history...'");
+        assertEquals("Checking history...\n", outContent.toString(), "checkHistory() should print 'Checking history...'");
     }
 
     @Test
     public void testCheckBudget() {
         FinTrack.checkBudget();
-        assertEquals("Checking budget...\n",
-                outContent.toString(),
-                "checkBudget() should print 'Checking budget...'");
+        assertEquals("Checking budget...\n", outContent.toString(), "checkBudget() should print 'Checking budget...'");
     }
 
     @Test
     public void testExit() {
 
         FinTrack.exit();
-        assertEquals("Exiting...\n",
-                outContent.toString(),
-                "exit() should print 'Exiting...'");
+        assertEquals("Exiting...\n", outContent.toString(), "exit() should print 'Exiting...'");
     }
 
     @Test
     public void testHandleInput_validCommand() {
         FinTrack.handleInput("1");
-        assertEquals("Adding expense...\n",
-                outContent.toString(),
-                "Input '1' should trigger handleExpense()");
+        assertEquals("Adding expense...\n", outContent.toString(), "Input '1' should trigger handleExpense()");
     }
 
     @Test
     public void testHandleInput_invalidCommand() {
         FinTrack.handleInput("invalid");
-        assertEquals("Unknown command: invalid\n",
-                outContent.toString(),
-                "Invalid input should print an error message.");
+        assertEquals("Unknown command: invalid\n", outContent.toString(), "Invalid input should print an error message.");
     }
 }
